e8ba9d6ec092e7cb2fca219c8893358e8d99ae00
Ryan O'Toole <ryanotoole@Ryans-MBP.attlocal.net>
2022-05-10 18:18:51 -0500

    integrated ethers metamask and the frontend is connecting to it

--- pages/Grid.js
+++ pages/Grid.js
@@ -1,16 +1,18 @@
 import styles from '../styles/Grid.module.css';
+import { Form } from 'semantic-ui-react';
+
 function Grid() {
     return (
         <div className={styles.container}>
-            <div className={ `${styles.cell1} ${styles.cell}` }>1.</div>
-            <div className={ `${styles.cell2} ${styles.cell}` }>2.</div>
-            <div className={ `${styles.cell3} ${styles.cell}` }>3.</div>
-            <div className={ `${styles.cell4} ${styles.cell}` }>4.</div>
-            <div className={ `${styles.cell5} ${styles.cell}` }></div>
-            <div className={ `${styles.cell6} ${styles.cell}` }>6.</div>
-            <div className={ `${styles.cell7} ${styles.cell}` }>7.</div>
-            <div className={ `${styles.cell8} ${styles.cell}` }>8.</div>
-            <div className={ `${styles.cell9} ${styles.cell}` }>9.</div> 
+            <div className={`${styles.cell} ${styles.cell1}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell2}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell3}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell4}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell5}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell6}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell7}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell8}`}>Hi man</div>
+            <div className={`${styles.cell} ${styles.cell9}`}>Hi man</div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,63 @@
-import styles from '../styles/Home.module.css'
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Container } from 'semantic-ui-react';
+import { Magic } from 'magic-sdk';
+import { useRouter } from 'next/router';
+import styles from '../styles/Login.module.css'
 
-class Home extends Component {
 
-  constructor(props) {
-    super(props);
+function Home(props) {
+
+  const [email, setEmail] = useState('');
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [userData, setUserData] = useState('');
+  const router = useRouter();
+
+  const handleClick = async () => {
+    console.log('top of handleClick', isLoggedIn);
+    if (isLoggedIn) {
+      const magic = new Magic('pk_live_72B536E564E8019B');
+      console.log('so true?')
+      await magic.user.logout();
+      const isLoggedIn = await magic.user.isLoggedIn();
+      setLoggedIn(isLoggedIn);
+      console.log('isLoggedIn', isLoggedIn);
+      setUserData('');
+    }
+    else {
+      const magic = new Magic('pk_live_72B536E564E8019B');
+      let redirectAddress = 'localhost:3000'
+      await magic.auth.loginWithMagicLink({ email, redirectAddress })
+      const isLoggedIn = await magic.user.isLoggedIn();
+      console.log('isLoggedIn', isLoggedIn);
+      setLoggedIn(isLoggedIn);
+      const userMetadata = await magic.user.getMetadata();
+      setUserData(userMetadata.email);
+      router.push('/User');
+
+    }
   }
-
-  handleClick = () => {
-    console.log('hi');
-  }
-
-  render() {
-    return (
-        <div>
-          Hye yo home page
-          <Button onClick={this.handleClick}>Click Me</Button>
-        </div>
-    );
-  }
+  
+  return (
+            <Form className={styles.login}>
+              <Form.Field>
+                <label className={styles.signUp}>Sign up</label>
+                <label className={styles.bodyText}>Can you enter your email address and we will send magic link to complete registation</label>
+                <input
+                  placeholder='Email Address:'
+                  value={email} 
+                  onChange={e => setEmail(e.target.value)}
+                  className={styles.input}
+                />
+              </Form.Field>
+              { isLoggedIn ? 
+              <button onClick={handleClick}>Logout</button>
+              :
+              <button className={styles.btn} onClick={handleClick}>Continue</button>
+              }  
+          </Form>
+  );
 }
+
 
 export default Home;
