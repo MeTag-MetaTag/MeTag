774b8b7b128dfff94cc93c382c23f3916077bc46
0xPr0f <0xprofoz@gmail.com>
2022-05-11 17:53:16 +0100

    🔄 edits

--- pages/User.js
+++ pages/User.js
@@ -1,71 +1,66 @@
-import React, { useState } from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Form, Container } from 'semantic-ui-react';
-import { Magic } from 'magic-sdk';
+import React, { useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import "../styles/styles.css";
+import { Button, Form, Container } from "semantic-ui-react";
+import { Magic } from "magic-sdk";
 // import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css'
-import { ethers } from 'ethers';
-
+import styles from "../styles/Home.module.css";
+import { ethers } from "ethers";
 
 const User = (props) => {
-
-  const [accounts, setAccounts] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [defaultAccount, setDefaultAccount] = useState('');
-  const [userBalance, setUserBalance] = useState('');
-  const [connectButtonText, setConnButtonText] = useState('Connect Wallet');
+  const [accounts, setAccounts] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [defaultAccount, setDefaultAccount] = useState("");
+  const [userBalance, setUserBalance] = useState("");
+  const [connectButtonText, setConnButtonText] = useState("Connect Wallet");
 
   const connectWalletHandler = async () => {
-      if(window.ethereum) {
-    //   let result = await window.ethereum.request({method: "eth_requestAccounts"});
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        let accounts = await provider.send("eth_requestAccounts", []);
-        console.log('accounts', accounts);
+    if (window.ethereum) {
+      //   let result = await window.ethereum.request({method: "eth_requestAccounts"});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      let accounts = await provider.send("eth_requestAccounts", []);
+      console.log("accounts", accounts);
 
       accountChangedHandler(accounts[0]);
-      }
-      else {
-          setErrorMessage('Install MetaMask');
-      }
-  }
+    } else {
+      setErrorMessage("Install MetaMask");
+    }
+  };
 
   const accountChangedHandler = (newAccount) => {
     setDefaultAccount(newAccount);
     getUserBalance(newAccount.toString());
-  }
+  };
 
   const getUserBalance = async (address) => {
-    const balance = await window.ethereum.request({ method: "eth_getBalance", params: [address, 'latest'] });
+    const balance = await window.ethereum.request({
+      method: "eth_getBalance",
+      params: [address, "latest"],
+    });
     setUserBalance(ethers.utils.formatEther(balance));
-  }
+  };
 
   const chainChangedHandler = () => {
     window.location.reload();
-  }
+  };
 
   if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-      window.ethereum.on('accountsChanged', accountChangedHandler);
-      window.ethereum.on('chainChanged', chainChangedHandler);
+    window.ethereum.on("accountsChanged", accountChangedHandler);
+    window.ethereum.on("chainChanged", chainChangedHandler);
   }
 
   return (
+    <div>
+      <div>Welcome User</div>
+      <Button onClick={connectWalletHandler}>Connect to MetaMask</Button>
       <div>
-          <div>Welcome User</div>
-          <Button onClick={connectWalletHandler}>Connect to MetaMask</Button>
-          <div>
-              <h3>Address: {defaultAccount}</h3>
-          </div>
-          <div>
-              <h3>Balance: {userBalance}</h3>
-          </div>
+        <h3>Address: {defaultAccount}</h3>
       </div>
+      <div>
+        <h3>Balance: {userBalance}</h3>
+      </div>
+    </div>
   );
-}
+};
 
 export default User;
-
-
-
-
-
-
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,86 @@
-import React, { useState } from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Form, Container } from 'semantic-ui-react';
-import { Magic } from 'magic-sdk';
-import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css'
-
+import React, { useState } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Form, Container } from "semantic-ui-react";
+import { Magic } from "magic-sdk";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 
 function Home(props) {
-
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState("");
   const router = useRouter();
 
   const handleClick = async () => {
-    console.log('top of handleClick', isLoggedIn);
+    console.log("top of handleClick", isLoggedIn);
     if (isLoggedIn) {
-      const magic = new Magic('pk_live_72B536E564E8019B');
-      console.log('so true?')
+      const magic = new Magic("pk_live_72B536E564E8019B");
+      console.log("so true?");
       await magic.user.logout();
       const isLoggedIn = await magic.user.isLoggedIn();
       setLoggedIn(isLoggedIn);
-      console.log('isLoggedIn', isLoggedIn);
-      setUserData('');
-    }
-    else {
-      const magic = new Magic('pk_live_72B536E564E8019B');
-      let redirectAddress = 'localhost:3000'
-      await magic.auth.loginWithMagicLink({ email, redirectAddress })
+      console.log("isLoggedIn", isLoggedIn);
+      setUserData("");
+    } else {
+      if (email == null) return;
+      const magic = new Magic("pk_live_72B536E564E8019B");
+      let redirectAddress = "localhost:3000";
+      await magic.auth.loginWithMagicLink({ email, redirectAddress });
       const isLoggedIn = await magic.user.isLoggedIn();
-      console.log('isLoggedIn', isLoggedIn);
+      console.log("isLoggedIn", isLoggedIn);
       setLoggedIn(isLoggedIn);
       const userMetadata = await magic.user.getMetadata();
       setUserData(userMetadata.email);
-      router.push('/User');
-
+      router.push("/User");
     }
-  }
-  
+  };
+
   return (
-            <Form className={styles.login}>
-              <Form.Field>
-                <label className={styles.signUp}>Sign up</label>
-                <label className={styles.bodyText}>Can you enter your email address and we will send magic link to complete registation</label>
-                <input
-                  placeholder='Email Address:'
-                  value={email} 
-                  onChange={e => setEmail(e.target.value)}
-                  className={styles.input}
-                />
-              </Form.Field>
-              { isLoggedIn ? 
-              <button onClick={handleClick}>Logout</button>
-              :
-              <button className={styles.btn} onClick={handleClick}>Continue</button>
-              }  
-          </Form>
+    <div className="bg">
+      <link
+        href="http://fonts.googleapis.com/css?family=Open+Sans"
+        rel="stylesheet"
+        type="text/css"
+      ></link>
+      <Form className="">
+        <div>
+          <br />
+          <h2>Sign Up</h2>
+          <div className="txt">
+            <span className="txtnorm">
+              Can you enter your email address and we will send
+            </span>
+            <span className="magiclinkbold"> magic link </span>
+            <span className="txtnorm">to complete registation</span>
+          </div>
+          <br />
+          <input
+            style={{ width: "260px", height: "45px" }}
+            type="email"
+            placeholder="Email Address:"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="txtinput"
+          />
+        </div>
+        <br />
+
+        {isLoggedIn ? (
+          <button
+            className=""
+            style={{ width: "20px", height: "20px" }}
+            onClick={handleClick}
+          >
+            Logout
+          </button>
+        ) : (
+          <button className="btn" onClick={handleClick}>
+            Continue
+          </button>
+        )}
+      </Form>
+    </div>
   );
 }
-
 
 export default Home;
