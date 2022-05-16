c6381e776a015d29c66018909d3162a23f1ac6c2
Ryan O'Toole <ryanotoole@Ryans-MBP.attlocal.net>
2022-05-16 18:57:21 -0500

    things working in a general sense

--- pages/User.js
+++ pages/User.js
@@ -1,63 +1,21 @@
+// import { useRouter } from 'next/router';
 import React, { useState } from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Button, Form, Container } from "semantic-ui-react";
 import { Magic } from "magic-sdk";
-// import { useRouter } from 'next/router';
-//import styles from "../styles/Home.module.css";
-import { ethers } from "ethers";
 
-const User = (props) => {
-  const [accounts, setAccounts] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [defaultAccount, setDefaultAccount] = useState("");
-  const [userBalance, setUserBalance] = useState("");
-  const [connectButtonText, setConnButtonText] = useState("Connect Wallet");
+const User = () => {
 
-  const connectWalletHandler = async () => {
-    if (window.ethereum) {
-      //   let result = await window.ethereum.request({method: "eth_requestAccounts"});
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      let accounts = await provider.send("eth_requestAccounts", []);
-      console.log("accounts", accounts);
 
-      accountChangedHandler(accounts[0]);
-    } else {
-      setErrorMessage("Install MetaMask");
-    }
-  };
 
-  const accountChangedHandler = (newAccount) => {
-    setDefaultAccount(newAccount);
-    getUserBalance(newAccount.toString());
-  };
 
-  const getUserBalance = async (address) => {
-    const balance = await window.ethereum.request({
-      method: "eth_getBalance",
-      params: [address, "latest"],
-    });
-    setUserBalance(ethers.utils.formatEther(balance));
-  };
+  async function connectWallet() {
 
-  const chainChangedHandler = () => {
-    window.location.reload();
-  };
-
-  if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-    window.ethereum.on("accountsChanged", accountChangedHandler);
-    window.ethereum.on("chainChanged", chainChangedHandler);
   }
 
   return (
     <div>
-      <div>Welcome User</div>
-      <Button onClick={connectWalletHandler}>Connect to MetaMask</Button>
-      <div>
-        <h3>Address: {defaultAccount}</h3>
-      </div>
-      <div>
-        <h3>Balance: {userBalance}</h3>
-      </div>
+      <div>started from the bottom now we're here</div>
     </div>
   );
 };
