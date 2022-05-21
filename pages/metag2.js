55345a9fa0b130c03dfd8502fb350db4cebf581a
0xPr0f <0xprofoz@gmail.com>
2022-05-21 09:19:38 +0100

    push

--- pages/metag2.js
+++ pages/metag2.js
@@ -142,45 +142,7 @@ function About(props) {
       console.error(error);
     }
   }
-  async function deleteFlow(recipient) {
-    const sf = await Framework.create({
-      networkName: "kovan",
-      provider: customHttpProvider,
-    });
 
-    const signer = sf.createSigner({
-      privateKey:
-        "0xd2ebfb1517ee73c4bd3d209530a7e1c25352542843077109ae77a2c0213375f1",
-      provider: customHttpProvider,
-    });
-
-    const DAIxContract = await sf.loadSuperToken("fDAIx");
-    const DAIx = DAIxContract.address;
-
-    try {
-      const deleteFlowOperation = sf.cfaV1.deleteFlow({
-        sender: "0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721",
-        receiver: recipient,
-        superToken: DAIx,
-        // userData?: string
-      });
-
-      console.log("Deleting your stream...");
-
-      await deleteFlowOperation.exec(signer);
-
-      console.log(
-        `Congrats - you've just deleted your money stream!
-       Network: Kovan
-       Super Token: DAIx
-       Sender: 0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721
-       Receiver: ${recipient}
-    `
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  }
   function calculateFlowRate(amount) {
     if (typeof Number(amount) !== "number" || isNaN(Number(amount)) === true) {
       alert("You can only calculate a flowRate based on a number");
