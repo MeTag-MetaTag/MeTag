3f38ed8184790829b4a36d2b4f3047dc718c37b8
Ryan O'Toole <ryanotoole@Ryans-MBP.attlocal.net>
2022-06-17 18:23:13 -0500

    i hate github

--- components/Navbar.js
+++ components/Navbar.js
@@ -36,6 +36,7 @@ function Navbar(props) {
   
 function installMetamask() {
 if (!(window.web3 || window.ethereum)) {
+  // pointless github workaround
   if ($('#installMetaMask').length < 1)
     $('.web3modal-modal-card').prepend('<div id="installMetaMask" class="cjAFRf web3modal-provider-wrapper"><a href="https://metamask.io/" target="_blank" class="cjAFRf web3modal-provider-container"><div class="jMhaxE web3modal-provider-icon"><img src="./assets/img/metamask.svg" alt="MetaMask"></div><div class="bktcUM sc-web3modal-provider-name mt-0">Install MetaMask</div><div class="eFHlqH web3modal-provider-description">Connect using browser wallet</div></a></div>')
 }
